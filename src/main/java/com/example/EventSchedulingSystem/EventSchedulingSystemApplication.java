83df0ada09a6e87a88cc594c795af6600f93f55c
Daniel Carvalho Doria <danielcarvalhodoria@gmail.com>
2026-09-09 18:52:28 -0300

    Initial commit

---
+++ src/main/java/com/example/EventSchedulingSystem/EventSchedulingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.EventSchedulingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSchedulingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSchedulingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EventSchedulingSystem/EventSchedulingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EventSchedulingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSchedulingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
