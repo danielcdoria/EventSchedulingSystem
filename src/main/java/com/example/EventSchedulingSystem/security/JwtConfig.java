26e51c1c6f4757f43375475766c09bd09b6f6666
Daniel Carvalho Doria <danielcarvalhodoria@gmail.com>
2026-09-10 18:08:31 -0300

    Update JwtConfig.java

--- src/main/java/com/example/EventSchedulingSystem/security/JwtConfig.java
+++ src/main/java/com/example/EventSchedulingSystem/security/JwtConfig.java
@@ -21,7 +21,7 @@ public class JwtConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/register", "/auth/login").permitAll().anyRequest().authenticated())
